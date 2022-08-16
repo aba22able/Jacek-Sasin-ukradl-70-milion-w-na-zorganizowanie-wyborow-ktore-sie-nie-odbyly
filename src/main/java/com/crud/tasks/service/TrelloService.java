bbd2d63e75bf58eb2f8c87664ddb6ad0a4d7770f
Albert Heesch <aheesch@op.pl>
2022-08-16 23:02:52 +0200

    23.4 bug fix 0.3

--- src/main/java/com/crud/tasks/service/TrelloService.java
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -33,7 +33,7 @@ public class TrelloService {
                         adminConfig.getAdminMail(),
                         SUBJECT,
                         "New card: " + trelloCardDto.getName() + " has been created on your Trello account",
-                        null
+                        "null"
                 )));
 
         return newCard;
