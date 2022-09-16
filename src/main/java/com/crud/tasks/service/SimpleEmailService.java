9b0162a2f47d9fda2c53bf95f9a00629d755255a
Albert Heesch <aheesch@op.pl>
2022-09-16 17:29:17 +0200

    Thymeleaf email message update

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -3,9 +3,12 @@ package com.crud.tasks.service;
 import com.crud.tasks.domain.Mail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 import static java.util.Optional.ofNullable;
 
@@ -15,6 +18,9 @@ import static java.util.Optional.ofNullable;
 public class SimpleEmailService {
 
     private final JavaMailSender javaMailSender;
+
+    @Autowired
+    private MailCreatorService mailCreatorService;
 
     public void send(final Mail mail) {
         log.info("Starting email preparation...");
@@ -40,5 +46,14 @@ public class SimpleEmailService {
         }
 
         return mailMessage;
+    }
+
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
     }
 }
